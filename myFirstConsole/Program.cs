c241ba3b8be67063647e018342830cb903347bcf
Paris B <parisbg@Pariss-MacBook-Pro-3.local>
2024-06-10 10:57:51 -0500

    Pushing coin flip code

---
+++ myFirstConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        string name = Register();
+        bool exit = false;
+        int count = 0;
+
+        while(!exit){
+            exit = CoinFlip(name, (count+1));
+            count++;
+        }
+
+        Console.WriteLine("Thanks for playing! \n");
+    }
+
+    public static bool CoinFlip(string name, int iteration){
+        Console.WriteLine("Flipping in progress...\n");
+
+        Random rand = new Random();
+        string flipResult;
+        string again;
+        int value = rand.Next();
+        int remainder = value % 2;
+    
+        flipResult = (remainder == 0) ? "Tails" : "Heads";
+        Console.WriteLine(name + "' Flip " + iteration + " Results = " + flipResult + "!");
+
+        Console.WriteLine("Would you like to flip again (y/n)? ");
+        again =  Console.ReadLine();
+
+        return (again == "n" || again == "N" || again == "no" || again == "NO" || again == "No");
+    }
+
+    public static string Register(){
+        string userName;
+        Console.WriteLine("Please your name: ");
+        userName =  Console.ReadLine();
+
+        return userName;
+    }
+
+
+}
