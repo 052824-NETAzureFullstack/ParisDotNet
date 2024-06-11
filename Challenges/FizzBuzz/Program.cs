2075bed52202f745a3ea271b9a197dc5154a9093
Paris B <parisbg@Pariss-MBP-3.lan>
2024-06-11 13:25:45 -0500

    Created realistic user menu with unique values

--- Challenges/FizzBuzz/Program.cs
+++ Challenges/FizzBuzz/Program.cs
@@ -2,7 +2,6 @@
 
 FizzBuzz - The frustratingly common FizzBuzz game.
 The player selects a range of numbers (2 - 18) and the computer plays FizzBuzz. 
-
 It prints the number on it's own line in the output. 
 If the number is divisible by 3, replace it with "Fizz". If the number is divisible by 5, replace with with "Buzz". 
 If the number is divisible by BOTH 3 and 5, replace with with "FizzBuzz". 
@@ -14,19 +13,25 @@ The player has to continue, either entering the word "Fizz", or selecting an opt
 
 using System;
 using System.ComponentModel;
+using System.Linq;
 
-public class FizzBuzz {
-    HashSet<string> userOptions = new HashSet<string>();
+
+public class FizzBuzz {    
+    public static List<string> userOptionsList;
 
     public static void Main(string[] args){
+        HashSet<string> userOptions = new HashSet<string>();
+
         (int start, int end) = UserSetRange();
 
         //First run is to fill a Hashset (so no duplicates) with realistic options for user selection
-        SimulateRun(start,end,false);
+        userOptions = SimulateRun(start,end,false);
+
+        //Convert our non-duplicate options into a list for easier random indexing.
+        userOptionsList = userOptions.ToList();
 
         //This is the actual gameplay with user
         SimulateRun(start,end,true);
-
     }
     
     public static bool By3(int num){ return (num % 3 == 0) ? true : false;}
@@ -46,37 +51,56 @@ public class FizzBuzz {
         if ((min + max > 0) && (value < min || value > max)){
             Console.WriteLine("Input should be only an integer value between " + min + "-" + max + " (Inclusive)! Please Try Again...\n");
             return false;
-        } else{
-            return true;
-        }  
+        }
 
         return true;
     }
 
-    public static string UserPlayMenu(int i){
+    public static string UserPlayMenu(int i, string answer){
+        List<string> someList = new List<string>();
         string userInput;
-        int rando1 = new Random().Next(0,userOptions.Count);
-        int rando2 = new Random().Next(0,userOptions.Count);
-        int rando3 = new Random().Next(0,userOptions.Count);
-        string[] options = [userOptions[rando1],userOptions[rando2],userOptions[rando3]];
+     
+        someList.Add(i.ToString());
+        someList.Add(answer);
+
+        while (someList.Count <= 4){
+            int rando = new Random().Next(0,userOptionsList.Count);
+
+            if (userOptionsList[rando] != answer){
+                someList.Add(userOptionsList[rando]);
+            }
+        }
+
+        //Now we've got a list with 4 unique values: 2 dummy (but realistically possible vals), the answer, the number option
+        Console.WriteLine("***********Options***********" );
+
+
+        string[] shuffledOptions = [answer, userOptionsList[rando1],userOptionsList[rando2],userOptionsList[rando3]];
+
+
+        shuffledOptions[new Random().Next(0,3)]
+
+
+
 
 
         Console.WriteLine("***********Options***********" );
         Console.WriteLine("[0] - " + options[0]);
         Console.WriteLine("[1] - " + options[1]);
-        Console.WriteLine("[2] - " + options[2]);    
+        Console.WriteLine("[2] - " + answer);    
         Console.WriteLine("[3] - " + i.ToString());
-
 
         Console.Write("Please select a number option between 0-3: ");
         userInput = Console.ReadLine(); 
 
-        if (IsValidIntInput(userInput)){
+        if (IsValidIntInput(userInput, start)){
             return options[Int32.Parse(userInput)];
         }
     }
 
-    public static void SimulateRun(int start, int end, bool isUser){
+    public static HashSet<string> SimulateRun(int start, int end, bool isUser){
+        HashSet<string> userOptionsSet = new HashSet<string>();
+
         for (int i = start; i < end+1; i++){
             string output = "";
             if (By3(i)){output += "Fizz";}
@@ -88,15 +112,17 @@ public class FizzBuzz {
             }
 
             if (isUser){
-                //answerAsString = UserPlayMenu();
+                //answerAsString = UserPlayMenu(i,answer);
                 //Must compare user Answer with computer answer
                 // if true print user answer to console and go to next iteration
                 //if false game is over and print message for the user
                 //Console.WriteLine(output);
             } else {
-                userOptions.Add(output);
+                userOptionsSet.Add(output);
             }
         }
+
+        return userOptionsSet;
     }
 
     public static (int, int) UserSetRange(){
